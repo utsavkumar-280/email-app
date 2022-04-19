5c2bcca5336c3bb17dd8dc210485f97434451691
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-04-19 21:17:26 +0530

    feat: added header component

---
+++ component/Header/index.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import styles from "./header.module.css";
+
+const Header = () => {
+	const { asPath } = useRouter();
+
+	const isHome = asPath === "/";
+	const isUnread = asPath === "/unread";
+	const isFavorites = asPath === "/favorites";
+
+	console.log({ asPath });
+	return (
+		<header className={styles.mainContainer}>
+			<nav className={styles.mainContent}>
+				<p className={styles.navText}>Filter By:</p>
+				<Link href="/" passHref>
+					<button className={isHome ? styles.activeNavLink : styles.navLink}>
+						<a>All</a>
+					</button>
+				</Link>
+				<Link href="/unread" passHref>
+					<button className={isUnread ? styles.activeNavLink : styles.navLink}>
+						<a>Unread</a>
+					</button>
+				</Link>
+
+				<Link href="/favorites">
+					<button
+						className={isFavorites ? styles.activeNavLink : styles.navLink}
+					>
+						<a>Favorites</a>
+					</button>
+				</Link>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
