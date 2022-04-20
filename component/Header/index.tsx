9125b38c5d7589ab3ad027016eda6de138e5ad24
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-04-20 10:17:22 +0530

    refact: header

--- component/Header/index.tsx
+++ component/Header/index.tsx
@@ -3,36 +3,30 @@ import { useRouter } from "next/router";
 
 import styles from "./header.module.css";
 
+const pages = [
+	{ name: "Unread", route: "/" },
+	{ name: "Read", route: "/read" },
+	{ name: "Favorites", route: "/favorites" },
+];
+
 const Header = () => {
 	const { asPath } = useRouter();
 
-	const isHome = asPath === "/";
-	const isUnread = asPath === "/unread";
-	const isFavorites = asPath === "/favorites";
-
-	console.log({ asPath });
 	return (
 		<header className={styles.mainContainer}>
 			<nav className={styles.mainContent}>
 				<p className={styles.navText}>Filter By:</p>
-				<Link href="/" passHref>
-					<button className={isHome ? styles.activeNavLink : styles.navLink}>
-						<a>Unread</a>
-					</button>
-				</Link>
-				<Link href="/read" passHref>
-					<button className={isUnread ? styles.activeNavLink : styles.navLink}>
-						<a>Read</a>
-					</button>
-				</Link>
-
-				<Link href="/favorites">
-					<button
-						className={isFavorites ? styles.activeNavLink : styles.navLink}
-					>
-						<a>Favorites</a>
-					</button>
-				</Link>
+				{pages.map((page) => (
+					<Link href={page.route} passHref>
+						<button
+							className={
+								asPath === page.route ? styles.activeNavLink : styles.navLink
+							}
+						>
+							<a>{page.name}</a>
+						</button>
+					</Link>
+				))}
 			</nav>
 		</header>
 	);
