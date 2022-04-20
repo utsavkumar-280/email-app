2a2beeab5ebe51ca7ac332a56f4e6d0938857ef1
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-04-20 15:25:19 +0530

    feat: added fetch apis

--- redux/constant.ts
+++ redux/constant.ts
@@ -1,7 +1,11 @@
-const AUTH = "AUTH";
-const Status = {
-    PENDING: "PENDING",
-    REJECTED: "REJECTED",
-    FULFILLED: "FULFILLED",
+export const Status = {
+	idle: "IDLE",
+	pending: "LOADING",
+	rejected: "FAILURE",
+	fulfilled: "SUCCESS",
 };
-export { AUTH, Status };
+
+export const EMAIL_LIST_API = "https://flipkart-email-mock.vercel.app/";
+export const PAGINATED_EMAIL_LIST_API =
+	"https://flipkart-email-mock.vercel.app/?page=";
+export const EMAIL_BODY_API = "https://flipkart-email-mock.vercel.app/?id=";
